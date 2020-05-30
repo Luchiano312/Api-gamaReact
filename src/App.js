0c22a2d307fde0384792703b6c6c76aa85c3eff2
luchiano312 <luchiano312@hotmail.com>
2020-05-30 13:15:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './Routes';
+import './style.css';
+
+function App() { 
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
